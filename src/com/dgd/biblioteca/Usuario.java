aec1594096094a55ae101de425515b90cce2ffa1
diegogd04 <diego2612004@gmail.com>
2022-11-10 17:25:57 +0100

    Creación clase Usuario.

---
+++ src/com/dgd/biblioteca/Usuario.java
@@ -0,0 +1,48 @@
+package com.dgd.biblioteca;
+
+public class Usuario {
+
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private Integer telefono;
+    private String direccion;
+    private String poblacion;
+
+    public String getDni(){
+        return dni;
+    }
+    public void setDni(String dni){
+        this.dni = dni;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getApellidos(){
+        return apellidos;
+    }
+    public void setApellidos(String apellidos){
+        this.apellidos = apellidos;
+    }
+    public Integer getTelefono(){
+        return telefono;
+    }
+    public void setTelefono(Integer telefono){
+        this.telefono= telefono;
+    }
+    public String getDireccion(){
+        return direccion;
+    }
+    public void setDireccion(String direccion){
+        this.direccion = direccion;
+    }
+    public String getPoblacion(){
+        return poblacion;
+    }
+    public void setPoblacion(String poblacion){
+        this.poblacion = poblacion;
+    }
+}
